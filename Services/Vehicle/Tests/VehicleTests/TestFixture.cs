a04f08f01dd22aee0cca2c6b51de3615f1e844c6
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2023-05-18 23:51:45 +0300

    Added infrastructure for testing. First unit tests

---
+++ Services/Vehicle/Tests/VehicleTests/TestFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using AutoPark.Api;
+using AutoPark.Svc.Infrastructure;
+using AutoPark.Svc.Infrastructure.TestData;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+
+namespace VehicleTests
+{
+    public class TestFixture : IDisposable
+    {
+        private readonly TestServer _testServer;
+        public HttpClient Client { get; }
+        
+        public VehicleContext DbContext { get; }
+        
+        public TestFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<Startup>() // Replace 'Startup' with your application's startup class
+                .UseEnvironment("Testing");
+            
+            var testServer = new TestServer(builder);
+            Client = testServer.CreateClient();
+
+            var options = new DbContextOptionsBuilder<VehicleContext>()
+                .UseInMemoryDatabase("any connection string because TestDB").Options;
+
+            DbContext = new VehicleContext(options);
+
+            // seed InMemory Database with test data
+            VehicleInitializer.Initialize(DbContext);
+        }
+
+        public void Dispose()
+        {
+            _testServer?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
---
+++ Services/Vehicle/Tests/VehicleTests/VehicleServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AutoPark.Svc;
+using AutoPark.Svc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Vehicle.Contract;
+using Xunit;
+using Moq;
+
+namespace VehicleTests
+{
+    public class VehicleServiceTests : IClassFixture<TestFixture>
+    {
+        private readonly HttpClient _client;
+        private readonly VehicleContext _dbContext;
+        private IVehicleService _vehicleService;
+
+        public VehicleServiceTests(TestFixture fixture)
+        {
+            _client = fixture.Client;
+            _dbContext = fixture.DbContext;
+            _vehicleService = new VehicleService(_dbContext);
+        }
+
+        #region UnitTests
+
+        // [Fact]
+        // public async Task GetVehicle_EntityNotFound_ThrowException()
+        // {
+        //     // Arrange
+        //     // Arrange
+        //     var mockDbSet = new Mock<DbSet<AutoPark.Svc.Infrastructure.Entities.Vehicle>>();
+        //     mockDbSet.As<IQueryable<AutoPark.Svc.Infrastructure.Entities.Vehicle>>()
+        //         .Setup(x => x.Provider)
+        //         .Returns(new TestAsyncQueryProvider<AutoPark.Svc.Infrastructure.Entities.Vehicle>(new List<AutoPark.Svc.Infrastructure.Entities.Vehicle>().AsQueryable().Provider));
+        //
+        //     var mockContext = new Mock<VehicleContext>();
+        //     mockContext.Setup(x => x.Vehicles)
+        //         .Returns(() => null);
+        //
+        //     _vehicleService = new VehicleService(mockContext.Object);
+        //
+        //     // Act - Assert
+        //     await Assert.ThrowsAsync<Exception>(() => _vehicleService.GetVehicle(1));
+        // }
+
+        #endregion
+
+        #region IntegrationTests
+
+        [Fact]
+        public async Task GetVehicle_Exist_CorrectDto()
+        {
+            // Act
+            var vehicleDto = await _vehicleService.GetVehicle(1);
+            
+            Assert.NotNull(vehicleDto);
+            Assert.Equal(1, vehicleDto.Id );
+            Assert.Equal(90_000, vehicleDto.Cost );
+        }
+
+        [Fact]
+        public async Task GetVehicle_EntityNotFound_ThrowException()
+        {
+            await Assert.ThrowsAsync<Exception>(() => _vehicleService.GetVehicle(100));
+        }
+
+        #endregion
+        
+    }
+}
